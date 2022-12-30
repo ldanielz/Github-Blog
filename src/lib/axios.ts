14f5451221b85c039a4b639b5cbf7f25384ac940
Lizandro Daniel <ldaniel.eng@gmail.com>
2022-12-30 18:33:14 -0300

    Axios update

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: 'http://localhost:3000/api/',
+  baseURL: 'https://github-blog-er9f2z6yz-ldanielz.vercel.app/api/',
 })
 export const apiJson = axios.create({
   baseURL: 'http://localhost:3004/',
